b0c355605eca47b0919e4d3213fbecfbab404590
Jimmy Beaudoin <jim@rockon.services>
2014-11-24 20:01:10 -0500

    Create gcloud-mount-drive.sh

---
+++ gcloud-mount-drive.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x
+
+DATA_FOLDER=$1
+DEVICE=$2
+
+sudo mkdir /$DATA_FOLDER
+sudo mke2fs -F -F -O ^has_journal -t ext4 -b 4096 -E lazy_itable_init=0 $DEVICE
+sudo mount $DEVICE /$DATA_FOLDER
+sudo chmod a+w /$DATA_FOLDER
