93b63cca335b767eae87f96f9e2a3960646f043b
Jimmy Beaudoin <jim@rockon.services>
2014-11-24 21:45:22 -0500

    Update gcloud-drive-init.sh

--- gcloud-drive-init.sh
+++ gcloud-drive-init.sh
@@ -5,6 +5,7 @@ DATA_FOLDER=$1
 DEVICE=$2
 
 sudo mkdir /$DATA_FOLDER
-sudo mke2fs -F -F -O ^has_journal -t ext4 -b 4096 -E lazy_itable_init=0 $DEVICE
+sudo mke2fs -F -F -O ^has_journal -t ext4 -b 4096 -E lazy_itable_init=0 /dev/sdb
+# sudo mke2fs -F -F -O ^has_journal -t ext4 -b 4096 -E lazy_itable_init=0 $DEVICE
 sudo mount $DEVICE /$DATA_FOLDER
 sudo chmod a+w /$DATA_FOLDER
